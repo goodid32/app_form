d3b0937b23cda603605bbd608e23e454f7e1bd43
DICCE <goodid32@gmail.com>
2023-01-16 14:18:50 +0900

    2023-01-16 #3

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
-
+import datetime
 st.subheader('회원가입 폼')
 
 with st.form('my_form', clear_on_submit=True):
     st.info('다음 양식을 모두 입력 후 제출합니다.')
     uid = st.text_input('아이디', max_chars=12)
     uname = st.text_input('이름', max_chars=10)
+    upw = st.text_input('비밀번호', type='password')
+    upw_chk = st.text_input('비밀번호 확인', type='password')
+    ubd = st.date_input('생년월일', min_value=datetime(1930,1,1))
+
 
     submitted = st.form_submit_button('제출')
     if submitted:
